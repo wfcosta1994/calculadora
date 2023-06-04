28c3dc0e6f890167f9feb4725297f084b892aa0b
Waldeir Fernandes <waldeir.fernandes94@gmail.com>
2023-06-04 18:12:26 -0300

    Colocando a condição IF e corrigindo o texto da tabuada

--- tabuada.js
+++ tabuada.js
@@ -1,16 +1,20 @@
 function tabuada(){
     var primeironum = window.document.getElementById("primeironum")
     var segundonum = window.document.getElementById("segundonum")
-    var p = Number(primeironum.value)
-    var s = Number(segundonum.value)
-    x = 0
-    var r = ""
-    var res = window.document.getElementById("resposta")
-    res.innerHTML = `A resposta é: ${p} <br>` 
-    do{
-        r = p*x
-        res.innerHTML += `${p} x ${x} = ${r} <br>` 
-        x++
-    } while(x <= s)
-       
+    if(primeironum.value == 0 || segundonum.value == 0){
+    window.alert("Verifique os dados")
+    }
+    else{
+        var p = Number(primeironum.value)
+        var s = Number(segundonum.value)
+        x = 0
+        var r = ""
+        var res = window.document.getElementById("resposta")
+        res.innerHTML = `A tabuada é: ${p} <br>` 
+        do{
+            r = p*x
+            res.innerHTML += `${p} x ${x} = ${r} <br>` 
+            x++
+        } while(x <= s)
+    }
 }
